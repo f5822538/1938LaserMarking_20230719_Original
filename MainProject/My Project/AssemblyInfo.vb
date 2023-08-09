d7bc8515f8e3f5f1ed27fe8bc5f172c95711f4c8
Chia-Hsien Lin <f5822538@gmail.com>
2023-08-09 09:41:53 +0800

    修改組件的版本資訊:2.1.0.7
    修改組件的版本資訊:2.1.0.7

--- MainProject/My Project/AssemblyInfo.vb
+++ MainProject/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 指定為預設值:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.1.0.6")> 
-<Assembly: AssemblyFileVersion("2.1.0.6")> 
+<Assembly: AssemblyVersion("2.1.0.7")> 
+<Assembly: AssemblyFileVersion("2.1.0.7")> 
